46f36de5b90b3b30795450ab6226b72126cd3b37
Loewe111 <m.loewe111@gmail.com>
2022-12-12 22:18:55 +0100

    added blockly

--- index.js
+++ index.js
@@ -15,11 +15,34 @@ function saveJSON(object, fileName) {
 }
 
 function download(){
-    var object = {
-        name: document.getElementById("input-name").value,
-        description: document.getElementById("input-description").value,
-        hasTeams: document.getElementById("input-teams").value
-    }
-    var fileName = document.getElementById("input-game-id").value + ".json";
-    saveJSON(object, fileName);
+  console.log(Blockly.JavaScript.workspaceToCode(workspace));
+  // var object = {
+  //     name: document.getElementById("input-name").value,
+  //     description: document.getElementById("input-description").value,
+  //     hasTeams: document.getElementById("input-teams").value
+  // }
+  // var fileName = document.getElementById("input-game-id").value + ".json";
+  // saveJSON(object, fileName);
 }
+
+//Blockly Code
+var toolbox = null;
+var workspace = null;
+var theme = null;
+
+async function initBlockly(){
+  //load toolbox from json file
+  
+  await $.getJSON("theme.json", function(data){
+    theme = data;
+  });
+  
+  $.getJSON("toolbar.json", function(data){
+    toolbox = data;
+    workspace = Blockly.inject('blockly', {toolbox: toolbox, theme: theme});
+  });
+
+  
+}
+
+initBlockly();
